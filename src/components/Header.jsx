bc2e6956b21f97bc7472f27f5b3df30e34fc2c47
Muhammad Shahzaib <shahzaibkhan4582@gmail.com>
2025-06-25 21:04:30 +0500

    last

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,14 +3,14 @@ export default function Header() {
     <header className="flex justify-between items-center py-9">
       <div>
         <h1 className="text-2xl md:text-3xl font-bold mb-2">Shahzaib Khan</h1>
-        <p className="text-md md:text-lg text-[#e4fde1]">
+        <p className="text-sm md:text-lg text-[#e4fde1]">
           Full Stack Developer | React Enthusiast
         </p>
       </div>
       <img
         src="/images/shahzaib-profile.jpg"
         alt="Profile"
-        className="h-15 w-15 md:w-17 md:h-17 rounded-full object-cover  shadow-lg"
+        className="h-18 w-18 lg:w-20 lg:h-20 rounded-full border-3 border-white object-cover shadow-lg"
       />
     </header>
   );
